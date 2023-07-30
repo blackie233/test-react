382101425209a2b9c11c0723f6799dd87adec17a
bin <blackie233@163.com>
2023-07-30 10:47:31 +0800

    test

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      11111
       {/*<header className="App-header">*/}
-      {/*  <p>hello world</p>*/}
+        <div>App</div>
+        <p>hello world</p>
       {/*  <code>src/App.js</code>*/}
       {/*</header>*/}
     </div>
